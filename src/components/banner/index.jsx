8ccc1f1b8e97378cfee5ea02fc7bba7d7b16af5b
WildBarreto <wildsbsrreto@gmail.com>
2024-04-02 22:03:45 -0300

    banner details

--- src/components/banner/index.jsx
+++ src/components/banner/index.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import banner from "../../assets/Banner login.png";
 
 import { Carousel } from "../carousel";
 import "./styles.css";
 
 export function Banner() {
+  const [isActive, setIsActive] = useState(true); // Definindo o estado inicial como false
+
+  const handleClick = () => {
+    setIsActive(!isActive); // Alternando o estado ao clicar
+  };
+
   return (
     <div className="banner">
       <img src={banner} alt="" />
+      <div className="informs">
+        <button>Cursos</button>
+        <h1>Plataforma de cursos completa</h1>
+        <p>
+          Lorem ipsum nisl etiam himenaeos ligula augue vehicula gravida
+          tincidunt, etiam magna sapien gravida sodales sed vel pulvinar
+          suspendisse, morbi mi proin urna ornare posuere donec aptent. orci
+          vivamus primis fusce lacinia libero nostra aliquam vestibulum
+        </p>
+
+        <div className="dashed">
+          <div className={isActive ? "active" : ""} onClick={handleClick} />
+          <div className={isActive ? "active" : ""} onClick={handleClick} />
+          <div className={isActive ? "active" : ""} onClick={handleClick} />
+        </div>
+      </div>
+
       <Carousel />
     </div>
   );
